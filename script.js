fd07ceead195c83234d01873d94e0e303f1ac2e2
Eduardo Valencia <EduardoVG@proton.me>
2024-03-22 17:22:29 -0600

    Switch para cambiar texto

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const html = document.querySelector('html');
 const listaBotones = document.querySelectorAll('.app__card-button');
-const banner = document.querySelector('.app__image');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const tituloNegro = document.querySelector('.app__title-strong');
 
 botonEnfoque.addEventListener('click', () => {
-    cambiarContexto('enfoque')
-})
+    cambiarContexto('enfoque');
+    cambiarTitulo(`asddsaividad,<br>`,'gola');
+});
 
 botonCorto.addEventListener('click', () => {
-    cambiarContexto('descanso-corto')
-})
+    cambiarContexto('descanso-corto');
+});
 
 botonLargo.addEventListener('click', () => {
-    cambiarContexto('descanso-largo')
-})
+    cambiarContexto('descanso-largo');
+});
 
 
 function cambiarContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagenes/${contexto}.png`);
+
+    switch (contexto) {
+        case 'enfoque':
+            titulo.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong" >sumérgete en lo que importa.</strong>
+                `
+            break;
+        case 'descanso-corto':
+            titulo.innerHTML = `
+            ¿Qué tal tomar un respiro? <br>
+            <strong class="app__title-strong" >¡Haz una pausa corta!</strong>
+                `
+            break;
+        case 'descanso-largo':
+            titulo.innerHTML = `
+            Hora de volver a la superficie <br>
+            <strong class="app__title-strong" >Haz una pausa larga.</strong>
+            `
+            break;
+        default:
+            break;
+    }
 }
